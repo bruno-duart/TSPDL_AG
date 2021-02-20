db67bf683610b79f4a39b987d5dac93627ac6991
bruno-duart <53445564+bruno-duart@users.noreply.github.com>
2021-02-20 17:52:24 -0300

    TSPDL_AG2

---
+++ CalculaMedias.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for file in burma14 ulysses16 gr17 gr21 ulysses22 fri26 bayg29 gr48
+do
+    for desv in 10 25 50
+    do
+        for inst in {1..10}
+        do
+            echo "python < ResultadosAG_Artigo2/$file"_"$desv"_"$inst.txt"
+            ./mainAG < instances/$file'_'$desv'_'$inst'.txt' > ResultadosAG_Artigo2/"Resultados_$file"_"$desv"_"$inst.csv"
+        done
+    done
+done
--- EncontraMenor.py
+++ EncontraMenor.py
@@ -6,6 +6,7 @@ dataF = pd.read_csv(f'{sys.argv[1]}.csv')
 #print(dataF)
 df = dataF.nlargest(11,'MediaRes').tail(1)
 print(df)
+df.to_csv(f'ResultadosAG_Artigo2/Resultados{sys.argv[2]}.csv', mode='a')
 
 
 
