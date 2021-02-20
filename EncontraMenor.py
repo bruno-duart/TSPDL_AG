497f4f500ea2a86f99a2d26439a7c567bc1c044a
bruno-duart <53445564+bruno-duart@users.noreply.github.com>
2021-02-20 17:30:53 -0300

    TSPDL_AG 20/02/21 - EncontraMenor
    Finalização da EncontraMenor.py

--- EncontraMenor.py
+++ EncontraMenor.py
@@ -2,6 +2,11 @@ import pandas as pd
 import sys
 
 dataF = pd.read_csv(f'{sys.argv[1]}.csv')
-print(dataF)
+#dataF = pd.read_csv(f'ResultadosAG_Artigo2/Resultados_burma14_50_2.csv')
+#print(dataF)
+df = dataF.nlargest(11,'MediaRes').tail(1)
+print(df)
 
-print(dataF['MediaRes'].min())
+
+
+#.query(f'MediaRes == {dataF["MediaRes"].min()}')
